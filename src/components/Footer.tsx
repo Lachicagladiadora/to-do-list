56e2e47ebd9ee6a721449fbfe13e25049775e656
Lachicagladiadora <200379@unsaac.edu.pe>
2023-12-07 23:27:05 -0500

    feat: change styles of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,55 @@
+import {
+  faGithub,
+  faGithubAlt,
+  faGithubSquare,
+  faSquareGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faGitkraken } from "@fortawesome/free-brands-svg-icons/faGitkraken";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Footer = () => {
   return (
     <footer
       style={{
         width: "100%",
         display: "flex",
-        flexDirection: "column",
+        // flexDirection: "column",
         gap: "10px",
         background: "#0B0E0D",
         alignItems: "center",
+        justifyContent: "center",
         padding: "20px",
         color: "#d772a3",
         position: "absolute",
         bottom: "0px",
       }}
     >
-      <a
-        href="https://github.com/Lachicagladiadora"
-        target="_blank"
-        style={{ background: "#0B0E0D" }}
-      >
-        <img
-          src="../../public/github-logo.svg"
-          alt="github"
-          height={"28px"}
-          style={{ background: "#0B0E0D" }}
-        />
-      </a>
-
-      <p
+      <span
         style={{
           background: "#0B0E0D",
-          color: "#e6e6e6",
+          color: "#e6e6e69e",
         }}
       >
         by Lachicagladiadora
-      </p>
-      <p
+      </span>
+      <a
+        href="https://github.com/Lachicagladiadora"
+        target="_blank"
+        style={{ background: "#0B0E0D", fontSize: "30px", color: "#0b5d3e" }}
+      >
+        <FontAwesomeIcon
+          icon={faGithub}
+          style={{ background: "transparent" }}
+        />
+      </a>
+      <span
         style={{
           background: "#0B0E0D",
-          color: "#e6e6e6",
+          color: "#e6e6e69e",
         }}
       >
         2023
-      </p>
+      </span>
     </footer>
   );
 };
