a61bf625a46315566d8c72156779e2020e90a99f
Lachicagladiadora <dev.roseny@gmail.com>
2024-12-13 11:51:23 -0500

    chore: update styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,7 @@
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 type todoType = {
   content: string;
   done: boolean;
@@ -13,18 +17,24 @@ type TodoProps = {
 
 export const TodoItem = ({ todo, toggleTodo }: TodoProps) => {
   return (
-    <li style={{ listStyle: "none" }}>
-      <input
-        type="checkbox"
-        checked={todo.done}
-        onChange={() => toggleTodo(todo)}
-        style={{
-          accentColor: `${todo.done ? "#48c0ac" : "#2b2a2a"}`,
-          width: "20px",
-          height: "20px",
-        }}
-      />{" "}
-      {todo.content}
+    <li style={{ width: "100%", listStyle: "none" }}>
+      <label className="to-do-item">
+        <FontAwesomeIcon icon={faCircle} className="non-check" />
+        <FontAwesomeIcon icon={faCheckCircle} className="check" />
+        <input
+          type="checkbox"
+          checked={todo.done}
+          onChange={() => toggleTodo(todo)}
+          style={{
+            accentColor: `${todo.done ? "#48c0ac" : "#2b2a2a"}`,
+            width: "20px",
+            height: "20px",
+            display: "none",
+            // borderRadius: "20px",
+          }}
+        />{" "}
+        {todo.content}
+      </label>
     </li>
   );
 };
